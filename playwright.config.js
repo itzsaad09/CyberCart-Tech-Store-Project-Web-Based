ff523962f5711aead3529db904a381f3f4300f51
Hafiz Muhammad Saad <muhammad.saad9788753@gmail.com>
2026-06-20 01:14:47 +0500

    Fix playwright

--- playwright.config.js
+++ playwright.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: "list",
+  reporter: [["list"], ["html", { open: "never" }]],
   use: {
     actionTimeout: 10000,
     baseURL: process.env.BASE_URL || "https://tech-store-project-frontend.vercel.app",
